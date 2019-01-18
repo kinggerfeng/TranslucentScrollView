d9e08cddb63e1db264fc45477acf9244c98efc81
kinggerfeng <318501388@qq.com>
2019-01-18 11:29:59 +0800

    v1.0

---
+++ app/src/main/java/test/com/MainActivity.java
@@ -0,0 +1,92 @@
+package test.com;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import test.com.base.BaseActivity;
+import test.com.impl.ActionBarClickListener;
+import test.com.widget.TranslucentActionBar;
+import test.com.widget.TranslucentScrollView;
+
+/**
+ * Created by Administrator on 2016/12/16.
+ * email:303767416@qq.com
+ */
+
+public class MainActivity extends BaseActivity implements ActionBarClickListener, TranslucentScrollView.TranslucentChangedListener {
+
+    private TranslucentScrollView translucentScrollView;
+    private TranslucentActionBar actionBar;
+    private View zoomView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Android 5.0 以上 全透明
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            // 状态栏（以上几行代码必须，参考setStatusBarColor|setNavigationBarColor方法源码）
+            window.setStatusBarColor(Color.TRANSPARENT);
+            // 虚拟导航键
+//            window.setNavigationBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // Android 4.4 以上 半透明
+            Window window = getWindow();
+            // 状态栏
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 虚拟导航键
+//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        init();
+    }
+
+    private void init() {
+        actionBar = (TranslucentActionBar) findViewById(R.id.actionbar);
+        //初始actionBar
+        actionBar.setData("测试", 0, null, 0, null, null);
+        //开启渐变
+        actionBar.setNeedTranslucent();
+        //设置状态栏高度
+        actionBar.setStatusBarHeight(getStatusBarHeight());
+
+        translucentScrollView = (TranslucentScrollView) findViewById(R.id.pullzoom_scrollview);
+        //设置透明度变化监听
+        translucentScrollView.setTranslucentChangedListener(this);
+        //关联需要渐变的视图
+        translucentScrollView.setTransView(actionBar);
+        //设置ActionBar键渐变颜色
+        translucentScrollView.setTransColor(getResources().getColor(R.color.orange_dft));
+
+        zoomView = findViewById(R.id.lay_header);
+        //关联伸缩的视图
+        translucentScrollView.setPullZoomView(zoomView);
+    }
+
+    @Override
+    public void onLeftClick() {
+
+    }
+
+    @Override
+    public void onRightClick() {
+
+    }
+
+    @Override
+    public void onTranslucentChanged(int transAlpha) {
+        actionBar.tvTitle.setVisibility(transAlpha > 48 ? View.VISIBLE : View.GONE);
+    }
+
+}
